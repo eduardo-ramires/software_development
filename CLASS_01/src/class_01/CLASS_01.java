cd2d990ac152fd357f4c53e6a0531f89c190a959
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-08-05 12:42:50 -0300

    Adiciona aula 01

---
+++ CLASS_01/src/class_01/CLASS_01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package class_01;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class CLASS_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
